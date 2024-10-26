39d146b34d711ed46b8162e0019d309ba189361e
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-10-26 20:20:24 +0530

    Heading created

--- src/TokenGenerator.js
+++ src/TokenGenerator.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Box } from '@mui/material';
+
 
 const TokenGenerator = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
+    const [blueTokens, setBlueTokens] = useState([]);                                   
+    const [redTokens, setRedTokens] = useState([]);                                       
+    const [formData, setFormData] = useState({
+      numBlue: '',                                                                         
+      prefixBlue: '',
+      bluePerRow: '',
+      numRed: '',
+      prefixRed: '',
+      redPerRow: '',
+    });
 
-export default TokenGenerator
+
+    return (
+        <div style={{ 
+  
+            minHeight: '100vh', 
+            justifyContent: 'center', 
+            alignItems: 'center'
+          }}>
+
+            <Box sx={{ padding: 5, maxWidth: 500,  margin: 'auto', width: '100%'}}>
+            <Typography variant="h4" paddingBottom={2} gutterBottom align="center" fontWeight={700} color='#093560' >
+                Token Generator Application
+            </Typography>
+
+            {/* Here now Form code */}
+            
+
+
+
+            </Box>
+            </div>
+        );
+    };
+    
+    export default TokenGenerator;
+    
